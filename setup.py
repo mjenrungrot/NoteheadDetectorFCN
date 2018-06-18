567fc4d21090cce644f36fb030768b40cf8260a1
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-06-18 13:47:55 -0700

    Setup package

---
+++ setup.py
@@ -0,0 +1,19 @@
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sheet_id",
+    version="0.0.1",
+    author="Teerapat Jenrungrot",
+    author_email="mjenrungrot@hmc.edu",
+    description="Description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
