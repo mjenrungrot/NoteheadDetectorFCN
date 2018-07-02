2026f87f32142a96e16c95d75b3c5e73c311e543
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-07-02 11:58:52 -0700

    Add XML boxes functionality

--- sheet_id/utils/dataGenerator.py
+++ sheet_id/utils/dataGenerator.py
@@ -2,6 +2,8 @@ import numpy as np
 import keras
 import scipy.misc as misc
 from random import shuffle, randint
+import os
+import xml.etree.ElementTree
 
 class DataGenerator(keras.utils.Sequence):
     """
@@ -41,9 +43,9 @@ class DataGenerator(keras.utils.Sequence):
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
 
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
+        X, y, boxes = self.__data_generation(list_IDs_temp)
 
-        return X, y
+        return X, y, boxes
 
     def on_epoch_end(self):
         """
@@ -53,14 +55,65 @@ class DataGenerator(keras.utils.Sequence):
         if self.shuffle:
             np.random.shuffle(self.indexes)
 
+    def _load_xml_annotation(self, xml_path, limits=(None,None,None,None)):
+        """
+        Load XML file 
+
+        xml_path - path to XML file
+        limits = 4-tuple (x1,y1,x2,y2) specifying the regions to include bounding boxes
+        """
+        # Parse XML file
+        tree = xml.etree.ElementTree.parse(xml_path)
+
+        # Get all objects
+        objs = tree.findall('object')
+
+        # Create placeholders for bounding boxes
+        num_objs = len(objs)
+        boxes = []
+ 
+        # Get the size of the entire sheet
+        size = tree.find('size')
+        height, width = float(size.find('height').text), float(size.find('width').text)
+
+        # Get each bounding box object
+        for ix, obj in enumerate(objs):
+            bbox = obj.find('bndbox')
+            x1 = np.floor(float(bbox.find('xmin').text)*width).astype(np.int16)
+            y1 = np.floor(float(bbox.find('ymin').text)*height).astype(np.int16)
+            x2 = np.floor(float(bbox.find('xmax').text)*width).astype(np.int16)
+            y2 = np.floor(float(bbox.find('ymax').text)*height).astype(np.int16)
+
+            # Elimiate boxes outside of the intended region
+            if (limits[0] is not None) and (x2 < limits[0]): continue
+            if (limits[2] is not None) and (x1 > limits[2]): continue
+            if (limits[1] is not None) and (y2 < limits[1]): continue
+            if (limits[3] is not None) and (y1 > limits[3]): continue
+
+            new_x1 = x1 - limits[0]
+            new_y1 = y1 - limits[1]
+            new_x2 = x2 - limits[0] 
+            new_y2 = y2 - limits[1]
+
+            # Eliminate boxes whose center is outside
+            center = ((new_x1+new_x2)/2, (new_y1+new_y2)/2)
+            if center[0] < new_x1 or center[0] >= new_x2: continue
+            if center[1] < new_y1 or center[1] >= new_y2: continue
+
+            boxes.append([new_x1, new_y1, new_x2, new_y2])
+        
+        return boxes
+
     def __data_generation(self, list_IDs_temp):
         """
         Generate data containing batch_size samples
         # X : (n_samples, *dim, n_channels)
         # y : (n_samples, *dim, n_channels)
+        # boxes : list of boxes
         """
         X = np.empty((self.batch_size, *self.dim, self.n_channels))
         y = np.empty((self.batch_size, *self.dim, self.n_channels), dtype=int)
+        boxes = []
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
@@ -79,7 +132,12 @@ class DataGenerator(keras.utils.Sequence):
             img = img[coord_0:(coord_0+self.crop_size[0]), coord_1:(coord_1+self.crop_size[1])]
             annotation = annotation[coord_0:(coord_0+self.crop_size[0]), coord_1:(coord_1+self.crop_size[1])]
 
+            # load xml annotation
+            xml_path = os.path.splitext(ID.replace('/images_png/', '/xml_annotations/'))[0] + '.xml'
+            boxes_annotation = self._load_xml_annotation(xml_path, limits=(coord_1, coord_0, coord_1 + self.crop_size[1], coord_0 + self.crop_size[0]))
+
             X[i,:] = img
             y[i,:] = annotation
+            boxes.append(boxes_annotation)
 
-        return X, y
+        return X, y, boxes
